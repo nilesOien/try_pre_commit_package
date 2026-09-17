bdec22805c47dc2ecf2d743c5f853c60cc85acc1
Niles Oien <nilesoien@gmail.com>
2026-09-17 13:26:39 -0600

    Added pydantic checking

--- the_fast_api.py
+++ the_fast_api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Set up tags that appear in the documentation pages that FastAPI generates.
 tags_metadata = [
@@ -41,7 +42,28 @@ static_data = {
 }
 
 
-@theApp.get("/static-dict", tags=["static-dict-service"])
+# Pydantic class (inherits from BaseModel) that defines our response schema.
+# Lets the documentation be more detailed.
+class theResponseClass(BaseModel):
+    """
+    This defines the schema for the JSON response.
+    """
+
+    firstName: str
+    lastName: str
+    numPets: int
+    usesPiApproximation: float
+    likesAurorasTooMuch: bool
+
+
+# The actual service that serves out the static dict.
+# Usually the dict would come from a database or something like that.
+# As such it could be quite extensive (and often is in production systems).
+# Here, it's just the static dictionary static_data that conforms to the schema
+# defined by theResponseClass.
+@theApp.get(
+    "/static-dict", response_model=theResponseClass, tags=["static-dict-service"]
+)
 async def get_static_dict():
     """
     Returns the static_data dictionary as a JSON response.
